cd93867963d9b191f11f7ea44f0adb80605e39be
vot3tada <zindromed@bk.ru>
2023-03-11 15:00:43 +0300

    finish models bd

--- server/models/models.js
+++ server/models/models.js
@@ -3,6 +3,7 @@ const {DataTypes} = require('sequelize')
 
 const User = sequelize.define('user',{
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING},
     login: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
     role: {type: DataTypes.STRING, defaultValue: "USER"}
@@ -11,15 +12,30 @@ const Post = sequelize.define('post',{
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING},
     content: {type: DataTypes.STRING},
+    image: {type: DataTypes.STRING, allowNull: true},
     likes: {type:DataTypes.INTEGER},
-    creator_id: {type: DataTypes.INTEGER}
+    createdAt: {type: DataTypes.DATE, defaultValue: sequelize.literal('CURRENT_TIMESTAMP')},
 })
-const Tegs = sequelize.define('tegs', {
+const Like = sequelize.define('Like', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+const Teg = sequelize.define('tegs', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING}
 })
-const Tegs_posts = sequelize.define('tegs_posts',{
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    teg_id: {type: DataTypes.INTEGER},
-    post_id: {type: DataTypes.INTEGER}
-})
+const PostTeg = sequelize.define('post_teg', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+});
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+User.hasMany(Like);
+Like.belongsTo(User);
+
+Post.belongsToMany(Teg, { through: PostTeg });
+Teg.belongsToMany(Post, { through: PostTeg });
+
+sequelize.sync({ force: true })
+    .then(() => console.log('Таблицы успешно созданы'))
+    .catch(error => console.log(`Ошибка при создании таблиц: ${error}`));
