2070dbf2dd613c24438d919c04409d90de07002a
vot3tada <zindromed@bk.ru>
2023-03-14 14:13:36 +0300

    end postController.js

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -2,25 +2,52 @@ const {Post} = require('../models/models')
 const ApiError = require('../error/ApiError')
 const path = require('path')
 const uuid = require('uuid')
+const fs = require('fs')
 
 class PostController {
     async add(req, res, next) {
         try {
             const {title, content} = req.body
-            const {img} = req.files
-            let filename = uuid.v4() + ".jpg"
-            img.mv(path.resolve(__dirname, '..', 'static', filename))
-
-            const post = await Post.add({title, content, image: filename})
-
+            const {image} = req.files
+            if (image)
+            {
+                let filename = uuid.v4() + ".jpg"
+                image.mv(path.resolve(__dirname, '..', 'static', filename))
+                const post = await Post.create({title, content, image: filename})
+                return res.json(post)
+            }
+            const post = await Post.create({title, content})
             return res.json(post)
-        } catch (e) {
+        }
+        catch (e) {
             next(ApiError.badRequest(e.message))
         }
     }
 
-    async edit(req, res) {
-
+    async edit(req, res, next) {
+        try {
+            const {id} = req.params
+            const {title, content} = req.body
+            let post = await Post.findOne({where: {...(id ? {id: +id} : {})}})
+            post.title = title
+            post.content = content
+            const {image} = req.files
+            if (image)
+            {
+                if (post.image) fs.unlink(path.resolve(__dirname, '..', 'static', post.image), (err) => {
+                    if (err) throw err})
+                let filename = uuid.v4() + ".jpg"
+                image.mv(path.resolve(__dirname, '..', 'static', filename))
+                post.image = filename
+                await post.save()
+                return res.json(post)
+            }
+            await post.save()
+            return res.json(post)
+        }
+        catch (e) {
+            next(ApiError.notFound(e.message))
+        }
     }
 
     async getAll(req, res) {
@@ -31,14 +58,31 @@ class PostController {
 
 
     async getOne(req, res, next) {
+        try {
             const {id} = req.params
             let post = await Post.findOne({where: {...(id ? {id: +id} : {})}})
-            if (!post) return next(ApiError.notFound())
+            if (!post) throw ApiError.notFound()
             return res.json(post)
+        }
+        catch (e)
+        {
+            next(ApiError.notFound(e.message))
+        }
     }
 
-    async delete(req, res) {
-
+    async delete(req, res, next) {
+        try {
+            const {id} = req.params
+            let post = await Post.findOne({where: {...(id ? {id: +id} : {})}})
+            if (post.image) fs.unlink(path.resolve(__dirname, '..', 'static', post.image), (err) => {
+                if (err) throw err})
+            await post.destroy()
+            return res.json({})
+        }
+        catch (e)
+        {
+            next(ApiError.notFound(e.message))
+        }
     }
 }
 
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,42 +1,18 @@
 const {User} = require('../models/models')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 const ApiError = require('../error/ApiError')
-const { where } = require('sequelize')
-
-const generateJwt = (id, login, role) => {
-    return jwt.sign({id,login,role},
-    'SecretKey12345',
-    {expiresIn: '24h'})
-}
 
 
 class UserController {
-    async registration(req, res, next) {
-        const {name, login, password} = req.body
-        if (!name || !login || !password) return next(ApiError.badRequest('Некорректный логин или пароль'))
-        const candidate = await User.findOne({ where: {login}})
-        if (candidate) return next(ApiError.badRequest('Пользователь с таким логином уже существует'))
-        const hashPassword = await bcrypt.hash(password, 7)
-        const user = await User.create({name, login,password: hashPassword})
-        const token = generateJwt(user.id, user.login, user.role)
-        return res.json({token})
+    async registration(req, res) {
+
     }
 
-
     async login(req, res) {
-        const {login, password} = req.body
-        const user = await User.findOne({where: {login}})
-        if (!user) return next(ApiError.internal('Пользователь не найден'))
-        let comparePassword = bcrypt.compareSync(password, user.password)
-        if (!comparePassword) return next(ApiError.internal('Неправильный пароль'))
-        const token = generateJwt(user.id, user.login, user.role)
-        return res.json({token}) 
+
     }
 
     async auth(req, res) {
-        const token = generateJwt(req.user.id, req.user.login, req.user.role)
-        return res.json({token})
+        return res.json({message: 'ALL WORKING'})
     }
 }
 
