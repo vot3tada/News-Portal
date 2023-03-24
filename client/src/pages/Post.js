58a0cd12970158eeb34c7805c5919a0055914461
vot3tada <zindromed@bk.ru>
2023-03-24 10:42:54 +0300

    add logic to show post

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const Post = () => {
+    const [post, setPost] = useState([]);
+    getPosts().then((res) => {
+        setPosts(res.data)
+    })
     return (
         <div>
-            
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+            {
+                post.image?
+                    <img src={} />
+                    :
+                    ''
+            }
+
+
         </div>
     );
 };
